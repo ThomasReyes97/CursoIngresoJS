9ca15429a7426f902e080555da73a6ca44cfc836
JoaquinGramajo <J.gramajolencina@gmail.com>
2023-01-30 12:25:59 -0300

    guia uno ejercicios 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,26 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	//alert("ok");
+
+/*
+todo lo que esta entre los asteriscos
+no sirve para el codigo
+*/
+// marcar que almacenan para ser mas organizado 
+	//var edad, no vamos a usar
+	//var a1
+	let nombre;
+	//let nombreCompleto;  notacion lower camel case (nuevas palabras todoas con mayusculas)
+
+	nombre = prompt("ingrese un nombre")
+
+	//nombre = "Joaquin"; //asignacion de string
+	// necesita valores de ambos lados y sirve para darle un valor a una variable (let)
+
+	alert(nombre)
 }
+// (variable) = (valor)
+// es escribirun valor asignado por el usuario 
+// nombre se asigna automaticamente a  prompt
 

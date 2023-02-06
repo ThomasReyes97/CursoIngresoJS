900e89f7fdb587a8a682a9321f25580f291a0054
JoaquinGramajo <J.gramajolencina@gmail.com>
2023-02-06 07:25:46 -0300

    actividades integradoras sabado

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -2,13 +2,44 @@
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'
 Nombre: Gramajo Lencina Joaquin
-Division: C*/
+Division: C
+Se ingresará por id el valor de una compra de productos para el hogar.
+Se deberá agregar el iva (21%) y
+luego se deberá restar el 5% en concepto de compra mayorista.
+El programa deberá mostrar por alert importe bruto, el iva,
+el importe con iva, el valor del descuento y el total a pagar.
+
+
+*/
 function mostrar()
 {
+	let importBrut;
+	let iva;
+	let importIva;
+	let descuento;
+	let importTotal;
+	let mensaje 
+
+	importBrut = document.getElementById("txtIdNombre").value;
+	importBrut = parseInt(importBrut)
+
+	iva = importBrut * 21 / 100
+	importIva = importBrut + iva
+
+	descuento = importBrut * 5 / 100
+
+	importTotal = importIva - descuento
+
+	mensaje = "importe Bruto: " + importBrut
+	mensaje = mensaje + " iva: " + iva + " importe con iva: " + importIva
+	mensaje = mensaje + " descuento: " + descuento + " importe total: " + importTotal
+
+	alert(mensaje)
+	/*
 	var nombreIngresado; // variable
 	nombreIngresado = txtIdNombre.value;    
 	alert(nombreIngresado);
-
+	*/
 }
 
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,16 +3,62 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 
 Nombre:Gramajo Lencina Joaquin
 Div: C
-Ejercicio 5*/
+Ejercicio 5
+ejercicio integrador
+Se debe ingresar por prompt: razón social de la empresa y nombre del titular.
+El dni del titular y la fecha de inicio de actividades se ingresarán por id.
+ Mostrar por alert todos los datos ingresados y el cuit de la empresa
+sabiendo que el mismo está conformado por un código numérico según el tipo de persona
+(30 en el caso de empresas), un guión, el dni del titular, otro guión y un dígito verificador 
+(será un número aleatorio entre 0 y 9). Por ejemplo: 30-22666548-7*/
 
 function mostrar()
 {	
+
+	let razonSoc;
 	let nombre;
+	let dniTitular;
+	let fecha;
+	let mensaje;
+	let aleatorio;
+
+	razonSoc = prompt("ingrese su razon social");
+	nombre = prompt("ingrese su nombre");
+
+	dniTitular = document.getElementById("txtIdNombre").value;
+
+	fecha = document.getElementById("txtIdEdad").value;
+
+	aleatorio = Math.round(Math.random()*9)
+
+	mensaje = "Razon social: ";
+	mensaje = mensaje + razonSoc;
+	mensaje = mensaje + " Nombre: ";
+	mensaje = mensaje + nombre;
+	mensaje = mensaje + " Dni del titular: ";
+	mensaje = mensaje + dniTitular;
+	mensaje = mensaje + " fecha: ";
+	mensaje = mensaje + fecha;
+	mensaje = mensaje + " Cuil: 30-" + dniTitular + "-" + aleatorio;
+	
+	alert(mensaje)
+	
+
+
+	/*alert("Razon social: " + razonSocNombre + 
+		" Dni del titular: " + dniTitular +
+		" fecha: " + fecha + 
+		" cuil de empresa: 30-" + dniTitular + "-" + aleatorio  )*/
+
+
+
+
+	/*let nombre;
 	let edad;
 	
 	nombre = txtIdNombre.value;
 	edad = txtIdEdad.value;
 
-	alert("usted se llama " + nombre + " y tiene " + edad + " años");
+	alert("usted se llama " + nombre + " y tiene " + edad + " años");*/
 }
 

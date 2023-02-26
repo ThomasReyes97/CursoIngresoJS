2f6d64093185c54f7801742c99a35c0ff61ff28a
JoaquinGramajo <J.gramajolencina@gmail.com>
2023-02-26 17:20:55 -0300

    while 1-10 complete

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -15,38 +15,85 @@ function mostrar()
 	//declarar contadores y variables 
 	var respuesta;
 	var numeroIngresado;
-	var sumaPositivos
+	var sumaPositivos;
 	var sumaNegativos;
-	var contadorpositivos
-	var contadornegativos
-
-	while(isNaN == numeroIngresado){
-		numeroIngresado = prompt("Ingrese un numero")
-		numeroIngresado = parseInt(numeroIngresado)
-	}
+	var contadorPositivos;
+	var contadorNegativos;
+	var contadorCeros;
+	var contadorNumeroPares;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferenciaNegativosPositivos
 	
-	sumaNegativos = 0
-	sumaPositivos = 0
+	sumaNegativos = 0;
+	sumaPositivos = 0;
 
-	contadornegativos = 0
-	contadorpositivos = 0
+	contadorNegativos = 0;
+	contadorPositivos = 0;
+	contadorCeros = 0;
+	contadorNumeroPares = 0;
 
-	respuesta="no";
+	respuesta="si";
 
-	do{
+	while(respuesta == "si" || respuesta == "SI" || respuesta == "Si" ){ 
+
+		numeroIngresado = prompt("Ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
 		
-		while(isNaN == numeroIngresado){
-			numeroIngresado = prompt("Ingrese un numero")
-			numeroIngresado = parseInt(numeroIngresado)
+		while(isNaN(numeroIngresado)){
+			numeroIngresado = prompt("ERROR!! Ingrese un numero");
+			numeroIngresado = parseInt(numeroIngresado);
+		}
+
+		if(numeroIngresado > 0){ //pos
+			
+			sumaPositivos = sumaPositivos + numeroIngresado;
+
+			contadorPositivos = contadorPositivos + 1;
+
+			promedioPositivos = sumaPositivos / contadorPositivos
+
+		}else{
+			if(numeroIngresado < 0){ //neg
+
+				sumaNegativos = sumaNegativos + numeroIngresado;
+
+				contadorNegativos = contadorNegativos + 1;
+
+				promedioNegativos = sumaNegativos / contadorNegativos
+			}else{
+				if(numeroIngresado > -1 || numeroIngresado < 1 ){
+					
+					contadorCeros = contadorCeros + 1;
+				}
+			}
+
+		}
+		if(numeroIngresado % 2 == 0){
+			contadorNumeroPares = contadorNumeroPares + 1;
 		}
 		
-
-		
 		
 
-	}while(respuesta != "si")
+		diferenciaNegativosPositivos = sumaPositivos - sumaNegativos
 
+		respuesta = prompt("¿Desea ingresar otro numero? (si/no)");
+	}
 	
+	document.write ("<p><br> SUMA DE NUMEROS NEGATIVOS: </p>"+ sumaNegativos);
+	document.write ("<p><br> SUMA DE NUMEROS POSITIVOS: </p>"+ sumaPositivos);
+	document.write ("<p><br> CANTIDAD DE NUMEROS NEGATIVOS: </p>"+ contadorNegativos);
+	document.write ("<p><br> CANTIDAD DE NUMEROS POSITIVOS: </p>"+ contadorPositivos);
+	document.write ("<p><br> CANTIDAD DE CEROS: </p>"+ contadorCeros);
+	document.write ("<p><br> CANTIDAD DE NUMEROS PARES: </p>"+ contadorNumeroPares);
+	document.write ("<p><br> PROMEDIO DE POSITIVOS: </p>"+ promedioPositivos);
+	document.write ("<p><br> PROMEDIO DE NEGATIVOS: </p>"+ promedioNegativos);
+	document.write ("<p><br> DIFERENCIA ENTRE NEGATIVOS Y POSITIVOS: </p>"+ diferenciaNegativosPositivos);
+
+	var texto = document.getElementsByTagName("p");
 	
-	document.write("la suma de negativos es :"+sumaNegativos);
+	for (var i = 0; i < texto.length; i++) {
+		texto[i].style.backgroundColor = "violet";
+	}
+
 }//FIN DE LA FUNCIÓN
